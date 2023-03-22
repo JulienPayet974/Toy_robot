5fce3a134ec2444403238f7094451df43cbbc4b4
Jul974 <116519412+JulienPayet974@users.noreply.github.com>
2023-03-22 10:46:20 +0100

    Last commit

--- robot.clj
+++ robot.clj
@@ -1,1 +1,14 @@
-"hello world!" => "hello world!";
+; Fonction pour placer le robot sur la table
+(defn place [x y direction])
+
+
+; Fonction pour déplacer le robot 
+(defn move [robot])
+; Fonction pour faire tourner le robot 
+(defn rotate [robot direction])
+
+; Fonction pour afficher la position et la direction du robot
+(defn report [robot])
+
+; Fonction pour exécuter une commande 
+(defn run [robot command])
